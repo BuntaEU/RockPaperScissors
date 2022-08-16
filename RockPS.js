c71e93fb5901740ce287e9e79294fcf5f17529af
Andre Palladino <93155128+BuntaEU@users.noreply.github.com>
2022-08-16 16:48:25 +0200

    Setting up CSS
    Changes made to CSS and added new classes to HTML

--- RockPS.js
+++ RockPS.js
@@ -1,21 +1,19 @@
-let computerMove;
-let computerScore = 0;
-let playerScore = 0;
-let gameMessage;
+const itemButtons = document.querySelectorAll(".play");
+const rounds = document.querySelector(".round");
+const combatText = document.querySelector(".combat-text");
+const playAgainButton = document.querySelector(".play-again");
 
-//Buttons for player
-const playButtons = document.querySelectorAll(".play");
+let playerLives = 5;
+let compLives = 5;
+let round = 0;
 
-/* const rockButton = document.querySelector(".rock");
-const paperButton = document.querySelector(".paper");
-const scissorsButton = document.querySelector(".scissors"); */
-
-/* Used to capitalize important words */
-/* function (string) {
-	return string.charAt(0).toUpperCase() + string.slice(1);
-} */
-
-/* Randomizes the computers choice */
+//Counts the Rounds
+function countRounds() {
+	round += 1;
+	round.innerHTML = `Round: ${round}`;
+	return round;
+}
+//Randomizes Computer Choice
 function getComputerChoice() {
 	let randomNum = Math.floor(Math.random() * 3);
 	if (randomNum === 0) {
@@ -28,26 +26,7 @@ function getComputerChoice() {
 	return computerMove;
 }
 
-/* asks for the players selection */
-/* function playerSelection() {
-	let playerMove = window
-		.prompt("Enter for Rock, Paper or Scissors")
-		.toString()
-		.toLowerCase();
-	console.log(playerMove);
-	if (
-		playerMove == "rock" ||
-		playerMove == "paper" ||
-		playerMove == "scissors"
-	) {
-		return playerMove;
-	} else {
-		alert("You didn't type Rock, Paper or Scissors! Please try again!");
-		playerSelection();
-	}
-} */
-
-/* code for the rounds played */
+function countLives(playerMove, computerMove) {}
 function playRound() {
 	let playerMove;
 	playButtons.forEach((weapon) => {
