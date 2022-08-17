e369dff0783d627c3ee540ea514ade220624147a
Andre Palladino <93155128+BuntaEU@users.noreply.github.com>
2022-08-17 15:30:39 +0200

    Changes HTML CSS JS
    Finished up HTML CSS and working on JS

--- RockPS.js
+++ RockPS.js
@@ -15,21 +15,75 @@ function countRounds() {
 }
 //Randomizes Computer Choice
 function getComputerChoice() {
+	const compIcon = document.querySelector(".compIcon");
 	let randomNum = Math.floor(Math.random() * 3);
+	compIcon.classList.remove("rock", "paper", "scissors");
 	if (randomNum === 0) {
 		computerMove = "rock";
+		compIcon.classList.add("rock");
 	} else if (randomNum === 1) {
 		computerMove = "paper";
+		compIcon.classList.add("paper");
 	} else if (randomNum === 2) {
 		computerMove = "scissors";
+		compIcon.classList.add("scissors");
 	}
 	return computerMove;
 }
 
-function countLives(playerMove, computerMove) {}
+function countLives(playerMove, computerMove) {
+	const gameOutput = document.querySelector(".game-output");
+	const compPlayDiv = document.querySelector(".compPlay");
+
+	switch (true) {
+		case playerMove === computerMove:
+			combatText.innerText = `Hmm.. Two ${playerMove}s means a draw, so no lives were lost. Let's try again.`;
+			gameOutput.style.border = "4px solid #8070ac";
+			compPlayDiv.classList.remove(
+				"red-border",
+				"yellow-border",
+				"green-border"
+			);
+			compPlayDiv.classList.add("grey-border");
+			break;
+		case playerMove === "rock" && computerMove === "scissors":
+		case playerMove === "paper" && computerMove === "rock":
+		case playerMove === "scissors" && computerMove === "paper":
+			combatText.textContent = `Impressive attack! The enemy lost one life, because the great power of your ${playerMove} crushed his ${computerMove}!`;
+			gameOutput.style.border = "4px solid #62b49c";
+			compPlayDiv.classList.remove(
+				"red-border",
+				"yellow-border",
+				"grey-border"
+			);
+			compPlayDiv.classList.add("green-border");
+			compLives -= 1;
+			break;
+		default:
+			combatText.textContent = "loser";
+			gameOutput.style.border = "4px solid #b96b78";
+			compPlayDiv.classList.remove(
+				"grey-border",
+				"yellow-border",
+				"green-border"
+			);
+			compPlayDiv.classList.add("red-border");
+			playerLives -= 1;
+			break;
+	}
+	const lives = document.querySelector(".lives");
+	lives.innerText = `Your lives: ${playerLives} | Enemy's Lives: ${compLives}`;
+	return [playerLives, compLives];
+}
+function endGame(playerLives, compLives) {
+	if (playerHealth === 0 || compLives === 0) {
+		weaponsButtons.forEach(button);
+	}
+}
+
 function playRound() {
 	let playerMove;
-	playButtons.forEach((weapon) => {
+	itemButtons.forEach((weapon) => {
 		weapon.addEventListener("click", () => {
 			if (weapon.classList.contains("rock")) {
 				playerMove = "rock";
@@ -38,6 +92,9 @@ function playRound() {
 			} else {
 				playerMove = "scissors";
 			}
+			countRounds();
+			countLives(playerMove, getComputerChoice());
 		});
 	});
 }
+playRound();
